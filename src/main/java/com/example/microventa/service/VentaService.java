21e679d86196f0fbc9a774ebcf1ffdc4777fa2cf
Mauricio Mora <mauricios.mora@gmail.com>
2025-06-29 22:13:42 +1200

    Se modifica service por error al descontar stock

--- src/main/java/com/example/microventa/service/VentaService.java
+++ src/main/java/com/example/microventa/service/VentaService.java
@@ -1,8 +1,6 @@
 package com.example.microventa.service;
 
-import com.example.microventa.client.ProductoClient;
 import com.example.microventa.config.RabbitMQConfig;
-import com.example.microventa.model.DetalleVenta;
 import com.example.microventa.model.Venta;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -13,20 +11,14 @@ import java.util.List;
 public class VentaService {
 
     private final RabbitTemplate rabbitTemplate;
-    private final ProductoClient productoClient;
 
-    public VentaService(RabbitTemplate rabbitTemplate, ProductoClient productoClient) {
+    public VentaService(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
-        this.productoClient = productoClient;
     }
 
     public Venta registrarVenta(Venta venta) {
-        for (DetalleVenta detalle : venta.getDetalles()) {
-            productoClient.descontarStock(detalle.getProductoId(), detalle.getCantidad());
-        }
-
         rabbitTemplate.convertAndSend(
-            "",
+            "", // default exchange
             RabbitMQConfig.VENTAS_QUEUE,
             venta
         );
